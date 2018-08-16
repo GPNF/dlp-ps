72ae1329e9b155e1f27b849f6d283998cf065ff9
adarshs1 <adarshs1@GPTC16.bitwiseglobal.net>
2018-08-16 13:33:21 +0530

    Updated topic & subscription name

--- src/main/java/app/service/SyncPullService.java
+++ src/main/java/app/service/SyncPullService.java
@@ -13,10 +13,12 @@ import com.google.pubsub.v1.PullRequest;
 import com.google.pubsub.v1.PullResponse;
 import com.google.pubsub.v1.ReceivedMessage;
 
+import app.util.ExternalProperties;
+
 public class SyncPullService {
 
-	private final String projectId = "possible-haven-212003";
-	private final String subscriptionId = "sub1";
+	private final String projectId = ExternalProperties.getAppConfig("app.gc.project.id");
+	private final String subscriptionId = ExternalProperties.getAppConfig("app.gc.pubsub.subscription");
 
 	public List<ReceivedMessage> getReceivedMessages(int numOfMessages, boolean returnImmediately) throws IOException {
 		SubscriberStubSettings subscriberStubSettings = SubscriberStubSettings.newBuilder().build();
