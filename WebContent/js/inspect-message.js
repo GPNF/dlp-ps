5c4ecd62d176e8f61160f630cf781a54e9a4b460
AdarshSinghal <adarsh.singhal@outlook.com>
2018-08-18 17:01:42 -0700

    Publish Page improvements
    Added loading status for publish page
    Added dynamic topic loading

--- WebContent/js/inspect-message.js
+++ WebContent/js/inspect-message.js
@@ -1,44 +1,49 @@
-$(document).ready(function(){
-	
-	$('#inspect-btn').click(function(){
+$(document).ready(function() {
+
+	$('#inspect-btn').click(function() {
 		var message = $('#inspect-message').val();
-		if(message.length<1){
+		if (message.length < 1) {
 			return;
 		}
+
+		disableBtn('#inspect-btn');
 		inspect(message);
 	});
-	
-	var inspect = function(message){
-		
-		
-		
-		var jsondata = {"message":message};
-		
-		
-		$.ajax
-        (
-            {
-                url:'/inspect',
-                data:JSON.stringify(jsondata),
-                type:'post',
-                cache:false,
-                complete:function(data){
-                	console.log('Logging');
-                	console.log(data);
-                	if (status === 'error' || !data.responseText) {
-                        handleError();
-                    }
-                    else {
-                        var message = data.responseText;
-                        $('#message').val(message);
-                    }
-                	
-                	
-                	
-                }
-            }
-        );
-		
+
+	var inspect = function(message) {
+
+		var jsondata = {
+			"message" : message
+		};
+
+		$.ajax({
+			url : '/inspect',
+			data : JSON.stringify(jsondata),
+			type : 'post',
+			cache : false,
+			complete : function(data) {
+				if (status === 'error' || !data.responseText) {
+					console.log(data);
+					resetDeIdentifyButton();
+				} else {
+					var message = data.responseText;
+					$('#message').val(message);
+					resetDeIdentifyButton();
+				}
+			}
+		});
+	};
+
+	function disableBtn(btn) {
+		var loading = '<i class="fa fa-spinner fa-pulse fa-1x fa-fw"></i>'
+		$(btn).html(loading);
+		$(btn).attr('disabled', 'disabled');
+	};
+
+	function resetDeIdentifyButton(button) {
+		$('#inspect-btn').html('Inspect');
+		$('#inspect-btn').removeAttr('disabled');
 	}
-	
+	;
+
 });
