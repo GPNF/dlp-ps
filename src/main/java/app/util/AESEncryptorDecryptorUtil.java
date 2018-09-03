6241a422f471ebe4bb763e6be3d26200cca4764c
Aniruddha Sinha <aniruddhas@bitwiseglobal.net>
2018-09-03 16:25:20 +0530

    Made some changes to AES Encryptor Decryptor for user convinience

--- src/main/java/app/util/AESEncryptorDecryptorUtil.java
+++ src/main/java/app/util/AESEncryptorDecryptorUtil.java
@@ -5,41 +5,36 @@ import javax.crypto.Cipher;
 import org.apache.commons.codec.binary.Base64;
 
 public class AESEncryptorDecryptorUtil {
-	private String encryptedString;
-	private String decryptedString;
 	private KeyManip keyManipulation;
 
 	public AESEncryptorDecryptorUtil() {
 		this.keyManipulation = new KeyManip();
 	}
 
-	public void encrypt(String stringToEncrypt) {
+	public String encrypt(String stringToEncrypt) {
+		String encryptedString = "";
 		try {
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			// setting key with a default password
 			cipher.init(Cipher.ENCRYPT_MODE, this.keyManipulation.setKey());
-			this.encryptedString = Base64.encodeBase64String(cipher.doFinal(stringToEncrypt.getBytes("UTF-8")));
+			encryptedString = Base64.encodeBase64String(cipher.doFinal(stringToEncrypt.getBytes("UTF-8")));
 		} catch (Exception e) {
 			System.out.println("Error while encrypting: " + e.toString());
 		}
+		return encryptedString.trim();
+
 	}
 
-	public void decrypt(String strToDecrypt) {
+	public String decrypt(String strToDecrypt) {
+		String decryptedString = "";
 		try {
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
 			cipher.init(Cipher.DECRYPT_MODE, this.keyManipulation.setKey());
-			this.decryptedString = new String(cipher.doFinal(Base64.decodeBase64(strToDecrypt)));
+			decryptedString = new String(cipher.doFinal(Base64.decodeBase64(strToDecrypt)));
 		} catch (Exception e) {
 
 			System.out.println("Error while decrypting: " + e.toString());
 		}
-	}
-
-	public String getEncryptedString() {
-		return this.encryptedString.trim();
-	}
-
-	public String getDecryptedString() {
-		return this.decryptedString;
+		return decryptedString.trim();
 	}
 }
