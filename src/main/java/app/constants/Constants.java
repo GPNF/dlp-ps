d8f52802625bda8a0011a590f8163d1efc0a084a
amolp <amolp@GPTC15.bitwiseglobal.net>
2018-09-07 17:27:01 +0530

    Added date utility to persist and use pubsub date in publisher dao only

--- src/main/java/app/constants/Constants.java
+++ src/main/java/app/constants/Constants.java
@@ -13,6 +13,9 @@ public class Constants {
 
 	public static final String SUBSCRIPTION_ID = ExternalProperties.getAppConfig("app.gc.pubsub.subscription.s2");
 
+	public static final String APPLICATION_DATE_FORMAT =ExternalProperties.getAppConfig("app.default.dateformat");
+	public static final String APPLICATION_TIMEZONE = ExternalProperties.getAppConfig("app.default.timezone");
+	
 	public static final String TRUE = "true";
 	public static final String IN_PROGRESS = "In-progress";
 	public static final String DELIVERED = "Delivered";
