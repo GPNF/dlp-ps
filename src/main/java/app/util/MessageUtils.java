12a5c2fd9df2e7932e227dcc55ef142eccfe0205
adarshs1 <adarshs1@GPTC16.bitwiseglobal.net>
2018-09-07 19:32:51 +0530

    Added Date Utility

--- src/main/java/app/util/MessageUtils.java
+++ src/main/java/app/util/MessageUtils.java
@@ -3,7 +3,6 @@ package app.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 import com.google.protobuf.Timestamp;
@@ -26,21 +25,24 @@ import app.model.SubscriberMessage;
  */
 public class MessageUtils {
 
-	private static final String YYYY_MM_DD_HH_MM_SS_A_Z = "yyyy-MM-dd hh:mm:ss a z";
-
-	private static SubscriberMessage getSubscriberMessage(ReceivedMessage msg) {
-		String ackId = msg.getAckId();
-		PubsubMessage pubsubMsg = msg.getMessage();
+	/**
+	 *  Converter ReceivedMessage into SubscriberMessage
+	 * 
+	 * @param message
+	 * @return SubscriberMessage
+	 */
+	private static SubscriberMessage getSubscriberMessage(ReceivedMessage message) {
+		String ackId = message.getAckId();
+		PubsubMessage pubsubMsg = message.getMessage();
 		String msgId = pubsubMsg.getMessageId();
 		String data = pubsubMsg.getData().toStringUtf8();
 		String globaTxnId = pubsubMsg.getAttributesOrThrow("globalTransactionId");
 		Timestamp timestamp = pubsubMsg.getPublishTime();
 		long time = timestamp.getSeconds();
 
-		SimpleDateFormat formatter = new SimpleDateFormat(YYYY_MM_DD_HH_MM_SS_A_Z);
+		SimpleDateFormat formatter = DateUtility.getDefaultFormatter();
 		Date publishDate = new Date(time * 1000);
 
-		formatter.setTimeZone(TimeZone.getTimeZone("US/Eastern"));
 		String publishTime = formatter.format(publishDate);
 		Date today = new Date();
 		String pullTime = formatter.format(today);
